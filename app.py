b8c1b04dce3f86f8ef0f054fc5ea8ba0960c584f
dots_13 <stasya.dots@gmail.com>
2023-08-26 05:01:39 +0200

    selectbox

--- app.py
+++ app.py
@@ -72,10 +72,25 @@ def main():
 
     feature_list = ['Gas', 'Flaring', 'Oil', 'Cement', 'Co2', 'Coal', 'Other industry']
     st.title('CO2 Emissions Forecasting App')
-    features = list(df.columns)
+
     selected_feature_box = st.selectbox(
         'Select your Industry',
         feature_list)
+
+    st.markdown(
+        """
+        <style>
+        [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+        width: 500px;
+        }
+        [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+        width: 500px;
+        margin-left: -500px;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
     st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: start;} </style>',
              unsafe_allow_html=True)
