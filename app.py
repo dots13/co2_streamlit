e7026a4586048b1e55abe4f5d92f9333db1a0b9e
dots_13 <stasya.dots@gmail.com>
2023-08-26 04:31:03 +0200

    v2

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import pickle
+import numpy as np
 from pathlib import Path
 import gdown
 from statsmodels.tsa.arima.model import ARIMAResults
@@ -14,9 +14,12 @@ rel_to_cwd_path = os.path.join(script_dir, rel_path)  # the cwd-relative path of
 
 # Read csv file
 df = pd.read_csv(rel_to_cwd_path, index_col='year')
-#df.drop(['land_use_change_co2', 'other_industry_co2'], axis=1, inplace=True)
+df.drop(['land_use_change_co2'], axis=1, inplace=True)
 # select values only after 1920
-df = df.loc['1920-01-01':]
+df = df.loc['1880-01-01':]
+df.loc[:'1950-01-01', 'flaring_co2'] = np.nan
+df.loc[:'1882-01-01', 'gas_co2'] = 0
+
 
 
 @st.cache_resource
@@ -42,13 +45,14 @@ color_palette = {
     'land_use_change_co2': '#549439'
 }
 
-color_palette_forecast = {
-    'gas_co2': '#89DFE6',
-    'flaring_co2': '#FFE659',
-    'oil_co2': '#3EFBB2',
-    'cement_co2': '#DFE9EB',
-    'co2': '#EF0000',
-    'coal_co2': '#927373'
+features_dic = {
+    'Gas': 'gas_co2',
+    'Flaring': 'flaring_co2',
+    'Oil': 'oil_co2',
+    'Cement': 'cement_co2',
+    'Co2': 'co2',
+    'Coal': 'coal_co2',
+    'Other industry': 'other_industry_co2',
 }
 
 models_dic = {}
@@ -66,49 +70,12 @@ for col in df.columns:
 
 def main():
 
-    print(models_dic)
+    feature_list = ['Gas', 'Flaring', 'Oil', 'Cement', 'Co2', 'Coal', 'Other industry']
     st.title('CO2 Emissions Forecasting App')
     features = list(df.columns)
-    selected_features = st.multiselect(
+    selected_feature_box = st.selectbox(
         'Select your Industry',
-        features)
-
-    st.markdown("""
-            <style>
-                span[data-baseweb="tag"][aria-label="gas_co2, close by backspace"]{
-                    background-color: #85DBF7;
-                    justify-content: start;
-                }
-                span[data-baseweb="tag"][aria-label="flaring_co2, close by backspace"]{
-                    background-color: #FFE760;
-                    justify-content: start;
-                }
-                span[data-baseweb="tag"][aria-label="oil_co2, close by backspace"]{
-                    background-color: #17C07F;
-                    justify-content: start;
-                }
-                span[data-baseweb="tag"][aria-label="cement_co2, close by backspace"]{
-                    background-color: #DFE9EB;
-                    justify-content: start;
-                }
-                span[data-baseweb="tag"][aria-label="co2, close by backspace"]{
-                    background-color: #EF0000;
-                    justify-content: start;
-                }
-                span[data-baseweb="tag"][aria-label="coal_co2, close by backspace"]{
-                    background-color: #6E6E6E;
-                    justify-content: start;
-                }
-                span[data-baseweb="tag"][aria-label="other_industry_co2, close by backspace"]{
-                    background-color: #F54C95;
-                    justify-content: start;
-                }
-                span[data-baseweb="tag"][aria-label="land_use_change_co2, close by backspace"]{
-                    background-color: #549439;
-                    justify-content: start;
-                }
-            </style>
-            """, unsafe_allow_html=True)
+        feature_list)
 
     st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: start;} </style>',
              unsafe_allow_html=True)
@@ -117,9 +84,10 @@ def main():
              unsafe_allow_html=True)
 
     radio_choose = st.radio("Choose plot option", ("Forecasted Values", "Whole Dataset"))
-    months = st.slider('Select the number of months for prediction:', 1, 96, value=1)
+    #months = st.slider('Select the number of months for prediction:', 1, 120, value=1)
 
-    forecasting_points = pd.date_range(start='2021-01-01', periods=months, freq="1M")
+    months=120
+    forecasting_points = pd.date_range(start='2021-01-01', periods=months, freq="1MS")
 
     fig = go.Figure()
     fig.update_xaxes(showline=True, linewidth=1, linecolor='rgb(96, 103, 117)', gridcolor='rgb(96, 103, 117)')
@@ -127,31 +95,43 @@ def main():
     fig.update_layout(paper_bgcolor="#262730", plot_bgcolor="rgb(52, 53, 56)")
 
     run_button = st.button('Run Forecast')
+    predicted_values = False
     if run_button:
-        for feature in selected_features:
-            if radio_choose == "Whole Dataset":
-                fig.add_trace(go.Scatter(x=df.index, y=df[feature],
-                                         mode='lines',
-                                         marker_color=color_palette[feature],
-                                         name=feature))
-                model = models_dic[feature]
-                predicted_values = model.forecast(months)
-                fig.add_trace(go.Scatter(x=forecasting_points, y=predicted_values,
-                                         mode='lines',
-                                         marker_color='red',
-                                         name=f"forecast {feature}",
-                                         showlegend=False)
-                            )
-            else:
-                model = models_dic[feature]
-                predicted_values = model.forecast(months)
-                fig.add_trace(go.Scatter(x=forecasting_points, y=predicted_values,
-                                         mode='lines',
-                                         marker_color=color_palette[feature],
-                                         name=feature))
+        selected_feature = features_dic[selected_feature_box]
+        if radio_choose == "Whole Dataset":
+            fig.add_trace(go.Scatter(x=df.index,
+                                     y=df[selected_feature],
+                                     mode='lines',
+                                     marker_color=color_palette[selected_feature],
+                                     name=selected_feature_box,
+                                     )
+                          )
+            model = models_dic[selected_feature]
+            predicted_values = model.forecast(months)
+            fig.add_trace(go.Scatter(x=forecasting_points, y=predicted_values,
+                                     mode='lines',
+                                     marker_color='red',
+                                     name='Forecast')
+                          )
+        else:
+            model = models_dic[selected_feature]
+            predicted_values = model.forecast(months)
+            fig.add_trace(go.Scatter(x=forecasting_points, y=predicted_values,
+                                     mode='lines',
+                                     marker_color=color_palette[selected_feature],
+                                     name=selected_feature,
+                                     )
+                          )
+        #fig.update_yaxes(ticksuffix="$")
+        fig.update_layout(title="CO2 Forecusting for 10 years.",
+                          yaxis_title='Million tonnes')
+        st.plotly_chart(fig, theme=None)
 
+        df_pred = pd.DataFrame(predicted_values).reset_index()
+        df_pred.columns = ['date', 'CO2 Million tonnes']
+        df_final = pd.DataFrame(df_pred.groupby(df_pred['date'].dt.year)['CO2 Million tonnes'].mean())
+        st.dataframe(df_final.T)
 
-    st.plotly_chart(fig, theme=None)
 
 if __name__ == '__main__':
     main()
