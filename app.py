2d54084d78e11e174a9de0d367dcf3540c4c6635
dots_13 <stasya.dots@gmail.com>
2023-08-27 21:21:53 +0200

    del importlib-metadata==6.8.0 from requirements

--- app.py
+++ app.py
@@ -77,20 +77,42 @@ def main():
         'Select your Industry',
         feature_list)
 
-    st.markdown(
-        """
-        <style>
-        [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
-        width: 500px;
-        }
-        [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
-        width: 500px;
-        margin-left: -500px;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+    st.markdown("""
+                <style>
+                    span[data-baseweb="tag"][aria-label="gas_co2, close by backspace"]{
+                        background-color: #85DBF7;
+                        justify-content: start;
+                    }
+                    span[data-baseweb="tag"][aria-label="flaring_co2, close by backspace"]{
+                        background-color: #FFE760;
+                        justify-content: start;
+                    }
+                    span[data-baseweb="tag"][aria-label="oil_co2, close by backspace"]{
+                        background-color: #17C07F;
+                        justify-content: start;
+                    }
+                    span[data-baseweb="tag"][aria-label="cement_co2, close by backspace"]{
+                        background-color: #DFE9EB;
+                        justify-content: start;
+                    }
+                    span[data-baseweb="tag"][aria-label="co2, close by backspace"]{
+                        background-color: #EF0000;
+                        justify-content: start;
+                    }
+                    span[data-baseweb="tag"][aria-label="coal_co2, close by backspace"]{
+                        background-color: #6E6E6E;
+                        justify-content: start;
+                    }
+                    span[data-baseweb="tag"][aria-label="other_industry_co2, close by backspace"]{
+                        background-color: #F54C95;
+                        justify-content: start;
+                    }
+                    span[data-baseweb="tag"][aria-label="land_use_change_co2, close by backspace"]{
+                        background-color: #549439;
+                        justify-content: start;
+                    }
+                </style>
+                """, unsafe_allow_html=True)
 
     st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: start;} </style>',
              unsafe_allow_html=True)
